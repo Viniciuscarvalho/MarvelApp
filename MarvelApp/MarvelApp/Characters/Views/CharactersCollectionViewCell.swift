73baad44dddb1a6f6b2c35db7c9f30c38d06915f
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2018-12-17 22:49:50 -0200

    Reestructure of heroes cell and add Metric for size of images

--- MarvelApp/MarvelApp/Characters/Views/CharactersCollectionViewCell.swift
+++ MarvelApp/MarvelApp/Characters/Views/CharactersCollectionViewCell.swift
@@ -7,54 +7,100 @@
 //
 
 import UIKit
+import SnapKit
 
 protocol FavoriteDelegate: class {
     func save(character: Character?)
 }
 
-class CharactersCollectionViewCell: UICollectionViewCell {
-
-    @IBOutlet weak var imageView: ImageViewAsync!
-    @IBOutlet weak var heroesTitle: UILabel!
-    @IBOutlet weak var favoriteIcon: UIImageView!
-    fileprivate var character: Character?
-    fileprivate var viewModel: CharactersViewModelProtocol?
+final class CharactersCollectionViewCell: UICollectionViewCell, CodeView {
     
-    fileprivate func clean() {
-        self.heroesTitle.text = ""
-        self.imageView.image = UIImage(named: "placeholder")
+    //Build elements on view
+    private let content: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = Metric.extraSmall
+        return view
+    }()
+    
+    private let photo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = Metric.extraSmall
+        return imageView
+    }()
+    
+    private let name: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
+    private let footer: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let favorite: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "Action/HeartOutline"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(didTouchAtFavorite), for: .touchUpInside)
+        return button
+    }()
+    
+    //Build view of snapkit
+    func buildHierarchy() {
+        addSubview(content)
+        content.addSubview(photo)
+        content.addSubview(footer)
+        content.addSubview(name)
+        content.addSubview(favorite)
     }
     
-    func setFavorite(character: Character) {
+    func buildConstraints() {
+        self.content.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+    
+    //Favorite functions
+    func favoriteAction(character: Character) {
         let id = "F\(character.id)"
         
         DispatchQueue.main.async {
             if let status = UserDefaults.standard.value(forKey: id) as? Bool {
                 if status {
-                    self.favoriteIcon.image = Assets.favoriteFull.image
+                    //self.favoriteIcon.image = Assets.favoriteFull.image
                 }
             }
         }
     }
     
-    override func prepareForReuse() {
-        self.imageView.image = Assets.logo.image
-        self.favoriteIcon.image = Assets.favoriteGray.image
+    func addFavoriteAction(action: @escaping () -> Void) {
+        favoriteAction = action
     }
     
+    private func installFavoriteAction() {
+        favorite.addTarget(self, action: #selector(didTouchAtFavorite), for: .touchUpInside)
+    }
+    
+    @objc private func didTouchAtFavorite() {
+        favoriteAction(character: character)
+    }
+
     func setup(character: Character, viewModel: CharactersViewModelProtocol? = nil) {
-        self.character = character
-        self.viewModel = viewModel
-        self.heroesTitle?.text = character.name
-        self.heroesTitle?.font = UIFont(name: "Avenir", size: 16)
-        if let path = character.thumbnail?.path, let ext = character.thumbnail?.extension {
-            self.imageView?.imageFromServerURL(urlString: "\(path).\(ext)")
-        }
-        self.setFavorite(character: character)
+//        self.character = character
+//        self.viewModel = viewModel
+//        self.heroesTitle?.text = character.name
+//        self.heroesTitle?.font = UIFont(name: "Avenir", size: 16)
+//        if let path = character.thumbnail?.path, let ext = character.thumbnail?.extension {
+//            self.imageView?.imageFromServerURL(urlString: "\(path).\(ext)")
+//        }
+        self.favoriteAction(character: character)
         
     }
-    
-    public func image() -> UIImage? {
-        return imageView.image
-    }
+
 }
---
+++ MarvelApp/MarvelApp/Shared/Extensions/Metric.swift
@@ -0,0 +1,25 @@
+//
+//  Metric.swift
+//  MarvelApp
+//
+//  Created by Vinicius Carvalho Marques on 17/12/18.
+//  Copyright © 2018 Vinicius Carvalho. All rights reserved.
+//
+
+import CoreGraphics
+
+struct Metric {
+    private static let multiplier: CGFloat = 4
+    /// Size: 4
+    static let extraSmall = multiplier
+    /// Size: 8
+    static let small = multiplier * 2
+    /// Size: 12
+    static let medium = multiplier * 3
+    /// Size: 16
+    static let large = multiplier * 4
+    /// Size: 20
+    static let extraLarge = multiplier * 5
+    /// Size: 24
+    static let doubleExtraLarge = multiplier * 6
+}
