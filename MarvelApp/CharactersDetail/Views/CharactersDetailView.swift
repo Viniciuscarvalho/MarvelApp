adfbc0deed11d332a781c2259fcde9b4504e8e43
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2019-02-26 23:01:34 -0300

    Add elements on detail view

--- MarvelApp/CharactersDetail/Views/CharactersDetailView.swift
+++ MarvelApp/CharactersDetail/Views/CharactersDetailView.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+class CharactersDetailView: UIView, CodeView {
+    
+    //Build elements on view
+    private let title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+    
+    private let text: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.lightGray
+        return label
+    }()
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        buildHierarchy()
+        buildConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func buildHierarchy() {
+        <#code#>
+    }
+    
+    func buildConstraints() {
+        <#code#>
+    }
+    
+    
+}
